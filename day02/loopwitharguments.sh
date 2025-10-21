1622fc8eae5261dab7c7b4b2658ba75dd2e9ada9
VENKATA RAMANA VALABOJU <159936794+venky9021@users.noreply.github.com>
2025-10-21 21:13:04 +0530

    Enhance comments and usage instructions in script
    Added comments and usage instructions for the script.

--- day02/loopwitharguments.sh
+++ day02/loopwitharguments.sh
@@ -4,6 +4,7 @@ here 1st argument is folder name
 2nd is starting number for the loop
 3rd is the ending number for the loop
 this is the command ./filename demo 01 10
+to remove all the directories which we have created now use rm -r hello*
 comments
 for(( num=$2 ; num<=$3 ; num++))
 do
